146990be673d35137ce5252064dce9472b985f77
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2023-01-15 16:46:32 +0100

    test: extend

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -4,15 +4,62 @@ import { minify } from 'csso';
 import sass from 'sass';
 import less from 'less';
 
-it('less and sass output should match', async () => {
-	const cLess = (
-		await less.render(fs.readFileSync('less/regular.less').toString(), {
+async function compileLess(weight: string): Promise<string> {
+	return (
+		await less.render(fs.readFileSync('less/' + weight + '.less').toString(), {
 			paths: ['less'],
-			filename: 'regular.less',
+			filename: weight + '.less',
 		})
 	).css;
+}
 
-	const cSass = sass.compile('sass/regular.scss').css;
+describe('compare less and sass output', () => {
+	it('thin', async () => {
+		const cLess = await compileLess('thin');
+		const cSass = sass.compile('sass/thin.scss').css;
 
-	expect(minify(cLess).css).toBe(minify(cSass).css);
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('light', async () => {
+		const cLess = await compileLess('light');
+		const cSass = sass.compile('sass/light.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('regular', async () => {
+		const cLess = await compileLess('regular');
+		const cSass = sass.compile('sass/regular.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('bold', async () => {
+		const cLess = await compileLess('bold');
+		const cSass = sass.compile('sass/bold.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('fill', async () => {
+		const cLess = await compileLess('fill');
+		const cSass = sass.compile('sass/fill.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('duotone', async () => {
+		const cLess = await compileLess('duotone');
+		const cSass = sass.compile('sass/duotone.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
+
+	it('index', async () => {
+		const cLess = await compileLess('index');
+		const cSass = sass.compile('sass/index.scss').css;
+
+		expect(minify(cLess).css).toEqual(minify(cSass).css);
+	});
 });
